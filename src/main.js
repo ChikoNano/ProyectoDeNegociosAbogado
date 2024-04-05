56aba2ae31175b16576c5ce60fd794627a943326
Rick Sanvicente <erick.sanvicente@gmail.com>
2024-04-05 16:41:45 -0600

    Se agrega la librería de primevue, para usar los componentes y estilos.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
 
-createApp(App).mount('#app')
+//in main.js
+import 'primevue/resources/themes/aura-light-green/theme.css'
+
+createApp(App)
+		.use(PrimeVue)
+		.mount('#app')
